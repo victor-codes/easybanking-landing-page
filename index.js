3b16dd62fb4bf5671ceb0e4e4b60b7fe511494fb
AYODEJI <ayodejiv5@gmail.com>
2020-11-28 14:27:42 +0100

    Update index.html, style.css, index.js and add remaining files.

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-const hiddenNavbar = document.querySelectorAll('.hidden-navbar')[0]
-const hamburgerOpen = document.querySelectorAll('.hamburger-icon')[0]
-const hamburgerClose = document.querySelectorAll('.hamburger-icon-close')[0]
-// if(document.body.clientWidth > 600) {
-//     document.querySelectorAll('.mobile')[0].style.display = 'none'
-//     document.querySelectorAll('.desktop')[0].style.display = 'block'
-//     console.log(document.body.clientWidth);
-// }
-hamburgerOpen.addEventListener('click', function () {
-    hamburgerClose.style.display = 'block'
-    hamburgerOpen.style.display = 'none'
-    setTimeout(function () {
-        hiddenNavbar.style.display = 'block'
-    }, 0)
+const hiddenNavbar = document.querySelector('.hidden-navbar');
+const hamburgerOpen = document.querySelector('.hamburger-icon');
+const hamburgerClose = document.querySelector('.hamburger-icon-close');
+
+hamburgerOpen.addEventListener('click', () => {
+    hamburgerClose.style.display = 'block';
+    hamburgerOpen.style.display = 'none';
+    hiddenNavbar.style.display = 'block';
+
     hiddenNavbar.animate([
         { opacity: `0` },
         { opacity: `1` }
     ],
         {
             duration: 400
-        })
-})
+        });
+});
+
 hamburgerClose.addEventListener('click', function () {
-    hamburgerClose.style.display = 'none'
-    hamburgerOpen.style.display = 'block'
-    setTimeout(function () {
-        hiddenNavbar.style.display = 'none'
-    }, 0)
+    hamburgerClose.style.display = 'none';
+    hamburgerOpen.style.display = 'block';
+    hiddenNavbar.style.display = 'none';
 })
